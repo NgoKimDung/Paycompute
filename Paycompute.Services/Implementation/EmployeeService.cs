09fa49aaf947b65da4c4a6b0b17a31e8d1ebb8a0
dung.ngo-thi-pham <duyphan@kpmg.com.vn>
2019-12-25 12:55:18 +0700

    Index - EmployeeController

---
+++ Paycompute.Services/Implementation/EmployeeService.cs
@@ -0,0 +1,60 @@
+﻿using Paycompute.Entity;
+using Paycompute.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Paycompute.Services.Implementation
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeService(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task CreateAsync(Employee newEmployee)
+        {
+            await _context.AddAsync(newEmployee);
+            await _context.SaveChangesAsync();
+        }
+
+        public Employee GetById(int employeeId) 
+            => _context.Employees.Where(x => x.Id == employeeId).FirstOrDefault();
+
+        public async Task Delete(int employeeId)
+        {
+            var employee = GetById(employeeId);
+            _context.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Employee> GetAll() => _context.Employees;
+
+        public async Task UpdateAsyc(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsyc(int id)
+        {
+            var employee = GetById(id);
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal UnionFees(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paycompute.Services;
+using Paycompute.Models;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            this._employeeService = employeeService;
+        }
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                ImageUrl = employee.ImageUrl,
+                DateJoined = employee.DateJoined,
+                Designation = employee.Designation,
+                City = employee.City
+            }).ToList();
+            return View(employees);
+        }
+    }
+}
